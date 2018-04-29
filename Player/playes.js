085f5d0e8ae1c2ea5f311c1938b56eacd2d774dc
palmix <qrrq.refai@gmail.com>
2018-04-30 00:36:47 +0300

    Update playes.js

--- Player/playes.js
+++ Player/playes.js
@@ -35,9 +35,9 @@ var setcvidd= b64DecodeUnicode(uncvidd);
 
 
 
-var setonechashid = b64DecodeUnicode(hashidonsite);
+var setonechashid = hashidonsite;
 var replacehash = setonechashid.replace("#","");
-var sethashids = setonechashid;
+var sethashids = "https://wuarabic.blogspot.com";
 
 var blob = new Blob([th+setcyt+setcem+vidyut], { type: 'text/plain' });
 var blobUrl = URL.createObjectURL(blob);
