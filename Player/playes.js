4b1db6194265fa533f595bc030c6beecf23c4959
palmix <qrrq.refai@gmail.com>
2018-04-30 01:11:16 +0300

    Update playes.js

--- Player/playes.js
+++ Player/playes.js
@@ -3,40 +3,32 @@
         var hopenpop = 450;
         var left = Number((screen.width/2)-(wopenpop/2));
         var tops = Number((screen.height/2)-(hopenpop/2));
-function b64EncodeUnicode(str) {
-return btoa( unescape( encodeURIComponent( str ) ) );
-}
-
-function b64DecodeUnicode(str) {
-	return decodeURIComponent( escape( atob( str ) ) );
-}
 
 var searchs = location.search;
-var hashidonsites = location.hash;
-var hashidonsite = hashidonsites;
+var hashidonsite = location.hash;
 var uncidvideourls = "Ly93dWFyYWIuYmxvZ3Nwb3QuY29tL3AvdmlkZW9zLmh0bWw/dmlkZW89";
-var idvideourls = b64DecodeUnicode(uncidvideourls);
+var idvideourls = Base64.decode(uncidvideourls);
 var idvideo = searchs.replace("?video=","");
-var vidyut= b64DecodeUnicode(idvideo);
+var vidyut= Base64.decode(idvideo);
 var idvideourl = idvideourls+idvideo;
 var uncyt = "eW91dHViZQ==";
-var setcyt= b64DecodeUnicode(uncyt);
+var setcyt= Base64.decode(uncyt);
 var uncem = "LmNvbS9lbWJlZC8=";
-var setcem= b64DecodeUnicode(uncem);
+var setcem= Base64.decode(uncem);
 var th = "https://www."
 var uncimg = "aHR0cHM6Ly9pLnl0aW1nLmNvbS92aS8="
-var setcimg= b64DecodeUnicode(uncimg);
+var setcimg= Base64.decode(uncimg);
 var uncvidy = "aHR0cHM6Ly93d3cueW91dHViZS5jb20vd2F0Y2g/dj0="
-var setcvidy= b64DecodeUnicode(uncvidy);
+var setcvidy= Base64.decode(uncvidy);
 var uncvidd = "aHR0cHM6Ly95Mm1hdGUuY29tL3lvdXR1YmUv"
-var setcvidd= b64DecodeUnicode(uncvidd);
+var setcvidd= Base64.decode(uncvidd);
 var uncvidd = "aHR0cHM6Ly95Mm1hdGUuY29tL3lvdXR1YmUv"
-var setcvidd= b64DecodeUnicode(uncvidd);
+var setcvidd= Base64.decode(uncvidd);
 
 
 
 
-var setonechashids = b64DecodeUnicode(hashidonsite);
+var setonechashids = Base64.decode(hashidonsite);
 var setonechashid = setonechashids;
 var replacehash = setonechashid.replace("#","");
 var sethashids = "https://wuarabic.blogspot.com"+replacehash;
