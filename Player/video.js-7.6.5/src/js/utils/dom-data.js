e4512750715d1e040e37b965efae046be970678f
palmix <qrrq.refai@gmail.com>
2019-09-29 01:48:21 +0300

    Add files via upload

---
+++ Player/video.js-7.6.5/src/js/utils/dom-data.js
@@ -0,0 +1,16 @@
+/**
+ * @file dom-data.js
+ * @module dom-data
+ */
+
+/**
+ * Element Data Store.
+ *
+ * Allows for binding data to an element without putting it directly on the
+ * element. Ex. Event listeners are stored here.
+ * (also from jsninja.com, slightly modified and updated for closure compiler)
+ *
+ * @type {Object}
+ * @private
+ */
+export default new WeakMap();
