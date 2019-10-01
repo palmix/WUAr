ba3c44d563c9c0f697482350ed228e019f3e23aa
palmix <qrrq.refai@gmail.com>
2019-10-01 18:01:24 +0300

    Add files via upload

---
+++ tools/didyouknow/diduknownew.js
@@ -0,0 +1,28 @@
+var ifgetcardinfo112 = setcardinfo11.getElementsByTagName("UL")[0];
+cardinfo11 = $(ifgetcardinfo112).html();
+
+cardinfo11 = cardinfo11.replace("<br>","")
+
+  $("#cardinfo").after("<center><div id='didyouknow'><p class='didyouknow0005'>هل تعلم</p><div class='didyouknow0004'></div><div class='owl-carousel owl-theme'>"+cardinfo11+"</div></div></center>"); 
+
+
+
+
+
+            $(document).ready(function() {
+              $('#didyouknow .owl-carousel').owlCarousel({
+                responsiveClass: true,
+           autoHeight:true,
+    rtl: true,
+    items:1,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:true,
+    stagePadding: 50,
+    loop:true,
+    margin:30,
+    nav:true,
+
+          
+              })
+            })
