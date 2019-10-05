a4704c3d679f964809d181f173cd019f763da4c9
palmix <qrrq.refai@gmail.com>
2019-10-05 23:14:02 +0300

    Update sharevid.js

--- tools/sharevid.js
+++ tools/sharevid.js
@@ -48,7 +48,6 @@ var videotracks = '';
      function changeVideo(videoID) {
       jwplayer('VAndD').setup({
        width: '100%',
-        height: '100%',
       skin: 'bekle',
       androidhls: 'true',
       stretching: 'exactfit',
