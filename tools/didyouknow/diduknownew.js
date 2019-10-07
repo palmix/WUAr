6b70de339deaa725103f5c79661472e5d7dff9c7
palmix <qrrq.refai@gmail.com>
2019-10-07 03:47:32 +0300

    Update diduknownew.js

--- tools/didyouknow/diduknownew.js
+++ tools/didyouknow/diduknownew.js
@@ -2,7 +2,7 @@ var setcardinfo11 = document.getElementById("cardinfo11");
 var ifgetcardinfo112 = setcardinfo11.getElementsByTagName("UL")[0];
 cardinfo11 = $(ifgetcardinfo112).html();
 
-cardinfo11 = cardinfo11.replace("<br>","")
+cardinfo11 = cardinfo11.replace("<br/>","")
 
   $("#cardinfo").after("<center><div id='didyouknow'><p class='didyouknow0005'>هل تعلم</p><div class='didyouknow0004'></div><div class='owl-carousel owl-theme'>"+cardinfo11+"</div></div></center>"); 
 
